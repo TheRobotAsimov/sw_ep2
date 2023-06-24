1f4ac303af2e45ff2a24961ee6d5befbe895e655
Isaiah <th3r0b0t4s1m0v@gmail.com>
2023-06-24 13:48:54 -0600

    Tabla en Index.vue

--- resources/js/Pages/Institutions/Index.vue
+++ resources/js/Pages/Institutions/Index.vue
@@ -1,74 +1,67 @@
-<template>
-    <!-- f -->
-    <h2>Api de Opciones Vue2</h2>
-    <p>brand:{{ brand }}</p>
-    <p>model:{{ model }}</p>
-    <p>color:{{ color }}</p>
-    <p>price:{{ price }}</p>
-    <p>power:{{ power }}</p>
-    <p>mensaje:{{ mensaje }}</p>
-    <br>
-    <button @click="upPower(10)" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-        Aumentar
-    </button>
-    <button @click="downPower(10)" type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
-        Disminuir
-    </button>
-    <br>
-    <br>
-    <br>
-    <h1>{{ titulo }}</h1>
-    <p>{{ Institution }}</p>
-
-</template>
-
 <script>
-import {ref} from "vue";
-export default {
-    props: {titulo: String, Institution: Object},
+import AppLayout from '@/Layouts/AppLayout.vue';
 
-    setup(){    // Vue3, todo dentro del método setup
-        const brand = "Audi";
-        const model = "A4";
-        const color = ["Azul", "Rojo", "Verde"]
-        const price = 3800;
-        let power = ref(180);
-        const mensaje = {
-            title: "Hola mundo",
-            text: "Adios",
-        };
-
-        //Método aumentar
-        const upPower = (newPower)=>{
-            power.value = power.value+newPower;
-        };  
-
-        //Método disminuir
-        const downPower = (newPower)=>{
-            power.value = power.value-newPower;
-        };
-
-        return{brand, model, color, price, power, mensaje, upPower, downPower};
-    }
-
-    /* data() { // Vue2, la variables se declaran dentro de data
-        return { // Defino variables
-            brand: "Audi",
-            model: "A4",
-            color: ["Azul", "Rojo", "Verde"],
-            price: 3800,
-            power: 180,
-            mensaje: {
-                title: "Hola mundo",
-                text: "Adios",
-            },
-        };
-    }, */
-
-    
+export default
+{   // Declarar
+    components: {
+        AppLayout,
+    },
+    props:{
+        titulo: String,
+        Institution: Object,
+    },
 };
 </script>
 
-<style>
+<template>
+    <AppLayout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                {{ titulo }}
+            </h2>
+        </template>
 
-</style>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="text-gray-200 dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+
+                    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3">
+                                        ID
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        NOMBRE DE LA INSTITUCIÓN
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        ACCIONES
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                                    <td class="px-6 py-4">
+                                        Silver
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        Laptop
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                                            Edit
+                                        </a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+    </AppLayout>
+</template>
